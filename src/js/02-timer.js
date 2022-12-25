5fff11699b3d576c9f157d783932fd87cd65bf41
AndriyNich <info@bembi.ua>
2022-12-26 00:12:59 +0200

    task-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,17 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const btnStart = document.querySelector('[data-start]');
+btnStart.setAttribute('disabled', 'true');
+
+flatpickr(document.querySelector('#datetime-picker'), options);

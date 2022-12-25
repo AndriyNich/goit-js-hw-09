346e7c8c0ae0f33bdcb87d44e803752252de5ab1
AndriyNich <info@bembi.ua>
2022-12-25 23:38:44 +0200

    task -1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+let isActive = false;
+let id = 0;
+
+document.querySelector('[data-start]').addEventListener('click', () => {
+  if (!isActive) {
+    isActive = true;
+    id = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+document.querySelector('[data-stop]').addEventListener('click', () => {
+  clearInterval(id);
+  isActive = false;
+});

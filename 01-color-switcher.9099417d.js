a9c5cbee34329efdbc1fed14be77470936f19c43
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2022-12-28 17:06:43 +0000

    Deploying to gh-pages from @ AndriyNich/goit-js-hw-09@6c60a36471e45eb40f5796053b74ff1067c24fae 🚀

--- 01-color-switcher.9099417d.js
+++
@@ -1,2 +0,0 @@
-let t=!1,e=0;document.querySelector("[data-start]").addEventListener("click",(()=>{t||(t=!0,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))})),document.querySelector("[data-stop]").addEventListener("click",(()=>{clearInterval(e),t=!1}));
-//# sourceMappingURL=01-color-switcher.9099417d.js.map

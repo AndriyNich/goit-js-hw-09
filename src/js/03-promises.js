39a75147fa61e4932eff23793acc2235cc60fd78
AndriyNich <info@bembi.ua>
2022-12-29 01:37:14 +0200

    finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,20 +3,29 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 Notiflix.Notify.init({ position: 'right-bottom' });
 
-function createPromise({ position, delay }) {
+function setDisabled() {
+  document.querySelector('button[type=submit]').setAttribute('disabled', true);
+}
+
+function setEnable(enable) {
+  if (enable)
+    document.querySelector('button[type=submit]').removeAttribute('disabled');
+}
+
+function createPromise({ position, delay, enable }) {
   return new Promise((resolve, reject) => {
     setTimeout(
-      ({ position, delay }) => {
+      ({ position, delay, enable }) => {
         const shouldResolve = Math.random() > 0.3;
 
         if (shouldResolve) {
-          resolve({ position, delay });
+          resolve({ position, delay, enable });
         }
 
-        reject({ position, delay });
+        reject({ position, delay, enable });
       },
       delay,
-      { position, delay }
+      { position, delay, enable }
     );
   });
 }
@@ -24,23 +33,31 @@ function createPromise({ position, delay }) {
 function formSubmit(event) {
   event.preventDefault();
 
-  const length = +document.querySelector("input[name='amount']").value;
-  const step = +document.querySelector("input[name='step']").value;
-  const delay = +document.querySelector("input[name='delay']").value;
+  setDisabled();
+
+  const length = +event.currentTarget.amount.value;
+  const step = +event.currentTarget.step.value;
+  const delay = +event.currentTarget.delay.value;
 
   for (let i = 0; i < length; i++) {
-    createPromise({ position: i + 1, delay: delay + i * step })
+    createPromise({
+      position: i + 1,
+      delay: delay + i * step,
+      enable: i + 1 === length,
+    })
       .then(onSucces)
       .catch(onError);
   }
 }
 
-function onSucces({ position, delay }) {
+function onSucces({ position, delay, enable }) {
   Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  setEnable(enable);
 }
 
-function onError({ position, delay }) {
+function onError({ position, delay, enable }) {
   Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  setEnable(enable);
 }
 
 document.querySelector('.form').addEventListener('submit', formSubmit);

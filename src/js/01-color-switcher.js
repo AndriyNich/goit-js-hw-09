1ab1344050bf66bad98d7a592dc43f5ce477304a
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2022-12-28 19:01:34 +0200

    All-pre-finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,19 +4,33 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-let isActive = false;
 let id = 0;
 
-document.querySelector('[data-start]').addEventListener('click', () => {
-  if (!isActive) {
-    isActive = true;
-    id = setInterval(() => {
-      document.body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+function btnSwitcher(isStart) {
+  if (isStart) {
+    refs.btnStart.setAttribute('disabled', 'true');
+    refs.btnStop.removeAttribute('disabled');
+  } else {
+    refs.btnStart.removeAttribute('disabled');
+    refs.btnStop.setAttribute('disabled', 'true');
   }
+}
+
+refs.btnStart.addEventListener('click', () => {
+  id = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnSwitcher(true);
 });
 
-document.querySelector('[data-stop]').addEventListener('click', () => {
+refs.btnStop.addEventListener('click', () => {
   clearInterval(id);
-  isActive = false;
+  btnSwitcher(false);
 });
+
+btnSwitcher(false);
